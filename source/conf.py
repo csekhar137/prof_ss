06197e11076af562a6d7fe0256b1cb956038a64a
chandrasekhar gopalakrishnan <53888065+csekhar137@users.noreply.github.com>
2019-10-08 13:10:23 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -47,8 +47,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
-
+html_theme = 'oe_sphinx'
+import oe_sphinx_theme
+html_theme_path = [oe_sphinx_theme.get_theme_dir()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
